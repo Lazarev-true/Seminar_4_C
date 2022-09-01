fcb86f42bfd13ffb8aa4d10098fed7416916d0f1
Lazarev Konstantin <klik@example.com>
2022-09-01 18:16:06 +0300

    Сумма цифр в числе

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе, которые являются делителями этого числа. 
+// (для получения цифр числа операцию приведения числа к строке 
+// не использовать)
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int num;
+int sum = 0;
+int value = number;
+
+while(value != 0)
+{
+    num = value % 10;
+    if(num != 0 && number % num == 0)
+    {
+        sum += num;
+    }
+    value /= 10;
+}
+
+Console.WriteLine($"{number} -> {sum}");
