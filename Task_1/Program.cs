407e0656be362cb40399871be8daa3cc115da35b
Lazarev Konstantin <klik@example.com>
2022-09-01 18:14:14 +0300

    А в степени В

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A 
+// в натуральную степень B. Не использовать 
+// функцию Math.Pow()
+
+Console.WriteLine("Введите целое число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число В");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int num = 1;
+
+for (int i = 1; i <= B; i++)
+{
+    num *= A;
+}
+Console.WriteLine($"{A}, {B} -> {num}");
